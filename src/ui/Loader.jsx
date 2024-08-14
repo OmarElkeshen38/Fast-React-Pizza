032177afdcf380dce4aeae417567aa98987dfd6b
omarelkeshen <oelkeshen5@gmail.com>
2024-08-14 04:00:20 +0300

    fetching data without navigation

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -3,7 +3,9 @@ function Loader() {
         <div className="absolute inset-0 bg-slate-200/20 backdrop-blur-sm
             flex items-center justify-center">
 
-            <div className="loader"></div>
+            <p>Loading...</p>
+
+            {/* <div className="loader"></div> */}
 
         </div>
     )
